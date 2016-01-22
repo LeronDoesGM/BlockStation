6e32346497264c4f6b1ae3e04b3a2ff2f01aa7c9
Häcker Felix <haecker.felix1207@gmail.com>
2016-01-22 19:59:37 +0100

    New 2.0 branch.

---
+++ src/blockStation/BlockStation.java
@@ -0,0 +1,10 @@
+package blockStation;
+
+public class BlockStation {
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+			System.out.println("BLOCKSTATION :3");
+	}
+
+}
